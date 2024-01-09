7595215cd02673824b2220bab5d9fe398b44aa16
Adrian <yy_1123@hotmail.com>
2024-01-09 18:03:19 +0800

    replit link update

--- src/features/listings/listingsSlice.js
+++ src/features/listings/listingsSlice.js
@@ -2,7 +2,8 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 
-const BASE_URL = "https://adrian-estate-api.yy1123.repl.co";
+//const BASE_URL = "https://adrian-estate-api.yy1123.repl.co";
+const BASE_URL = "https://d6648714-64f2-4a55-9297-ed6c0da3a482-00-2lkyyecz5uleu.riker.replit.dev"
 
 // fetch the 5 latest listings
 export const fetchLatestListings = createAsyncThunk(
